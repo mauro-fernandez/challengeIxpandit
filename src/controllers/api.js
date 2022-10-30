41d756be3562aab98ee9898866f7045db6f1dd47
Mauro <mauro.e.n.fernandez@gmail.com>
2022-10-30 14:38:15 -0300

    resolve warnings & improve performance

--- src/controllers/api.js
+++ src/controllers/api.js
@@ -2,37 +2,19 @@ import axios from 'axios';
 
 const baseUrl = "https://pokeapi.co/api/v2"
 
-// const apiJson = await axios.get(`${baseUrl}/pokemon?offset=0&limit=1154`)
-/*
-export const searchPokemon = async (pokemon) => {
-    try {
-        const apiJson = await axios.get(`${baseUrl}/pokemon/${pokemon}`)
-        // console.log(apiJson.data)
-        return apiJson.data
-        
-    } catch (error) {
-        console.log(error)
-    }
-}
-*/
-
-export const getApiInfo = async (limit = 1154, offset = 0) => {
+export const getApiInfo = async (limit = 649, offset = 0) => {
     try {
         const apiJson = await axios.get(`${baseUrl}/pokemon?offset=${offset}&limit=${limit}`)
-        // console.log(apiJson.data)
-        return  apiJson.data
-      
+        return apiJson.data      
     } catch (error) {
         console.log(error)
     }
   }
 
-  export const getOnePokemonData = async (url) => {
+export const getOnePokemonData = async (url) => {
     try {
         const apiJson = await axios.get(url)
-        // console.log(apiJson.data)
         return apiJson.data
-        
     } catch (error) {
         console.log(error)
     }
